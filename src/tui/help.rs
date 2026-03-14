65646957ee5baab706de862db5a6d317c05ac40f
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-03-14 18:12:11 +0100

    read help from file

--- src/tui/help.rs
+++ src/tui/help.rs
@@ -1,37 +1,4 @@
-pub const HELP_CONTENT: &str = r#"Keymaps
-
-General
-
-  <tab> / <S-tab>        move focus between panes
-  ?                      open or close help view
-  <Esc> / q              go back or quit
-  <Ctrl+c>               quit immediately
-
-Main View
-
-  [                      select previous session
-  ]                      select next session
-  {                      select previous theme
-  }                      select next theme
-  t                      toggle time display
-
-Sessions Pane
-
-  j / down               select next session
-  k / up                 select previous session
-  g                      select first session
-  G                      select last session
-  x                      archive selected session
-  J                      scroll crumbs down
-  K                      scroll crumbs up
-
-Crumbs Pane
-
-  j / down               scroll down
-  k / up                 scroll up
-  g                      scroll to the top
-  G                      scroll to the bottom
-"#;
+pub const HELP_CONTENT: &str = include_str!("assets/help.txt");
 
 pub fn help_line_count() -> usize {
     HELP_CONTENT.lines().count()
